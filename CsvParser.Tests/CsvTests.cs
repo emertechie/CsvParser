b9676d3e6613711992a148dded6cf9ab36f7cba0
Andrew Smith <devnull@localhost>
2012-07-29 13:54:40 +0100

    Get whitespace stipping working for unquoted values

--- CsvParser.Tests/CsvTests.cs
+++ CsvParser.Tests/CsvTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -21,6 +22,7 @@ namespace CsvParser.Tests
 			Assert.Equal("bbb", line[1]);
 		}
 
+		/*
 		// Note: Keeping it simple for now
 		[Fact]
 		public void WhiteSpaceIsNotRemovedFromAroundUnquotedValues()
@@ -36,10 +38,26 @@ namespace CsvParser.Tests
 			Assert.Equal(" aaa\t", line[0]);
 			Assert.Equal(" \t bbb ", line[1]);
 		}
+		*/
 
-		/*
 		[Fact]
-		public void WhiteSpaceIsRemovedFromAroundUnquotedValues()
+		public void SpacesAreRemovedFromAroundUnquotedValues()
+		{
+			var parser = new CsvParser();
+
+			IEnumerable<string[]> lines = parser.Parse(" a , b ");
+
+			Assert.Equal(1, lines.Count());
+			var line = lines.Single();
+
+			Assert.Equal(2, line.Length);
+			Console.WriteLine("Expected:'{0}', Actual:'{1}'", "a", line[0]);
+			Assert.Equal("a", line[0]);
+			Assert.Equal("b", line[1]);
+		}
+
+		[Fact]
+		public void TabsAreRemovedFromAroundUnquotedValues()
 		{
 			var parser = new CsvParser();
 
@@ -49,10 +67,10 @@ namespace CsvParser.Tests
 			var line = lines.Single();
 
 			Assert.Equal(2, line.Length);
+			Console.WriteLine("Expected:'{0}', Actual:'{1}'", "aaa", line[0]);
 			Assert.Equal("aaa", line[0]);
 			Assert.Equal("bbb", line[1]);
 		}
-		*/
 
 		[Fact]
 		public void WhitespaceInMiddleOfUnquotedValuesIsPreserved()
--- CsvParser/CsvParserModule.fs
+++ CsvParser/CsvParserModule.fs
@@ -3,24 +3,41 @@
 open FParsec
 open System
 
+let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
+    fun stream ->
+        printfn "%A: Entering %s" stream.Position label
+        let reply = p stream
+        printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
+        reply
+
 let ws = spaces
 let str s = pstring s
 let str_ws s = str s .>> ws
 
 let nonQuoteChars = manySatisfy (fun c -> c <> '"')
 let escapedQuote = str ("\"\"")
-let quotedField = between (str "\"") (str "\"") (stringsSepBy nonQuoteChars escapedQuote)
+let quotedField = between (str "\"") (str "\"") (stringsSepBy nonQuoteChars escapedQuote) <!> "quoted field"
 
-let unquotedField = manySatisfy (function ',' | '\n' -> false | _ -> true)
+// let unquotedField = manySatisfy (function ',' | '\n' -> false | _ -> true)
+let nonSpaceOrSep = manySatisfy (function ','|'\n'|' '|'\t' -> false | _ -> true) <!> "nonSpaceOrSep"
+let allowedSpaces = many1Satisfy (function ' '|'\t' -> true | _ -> false) <!> "allowedSpaces"
+// let allowedSpaces = manyChars (anyOf " \t")
+// let unquotedField = pipe2 nonSpaceOrSep (many allowedSpaces) (fun hd tl -> hd::tl |> List.reduce (+)) <!> "unquoted field"
+// let unquotedField = pipe2 nonSpaceOrSep (many allowedSpaces) (fun hd tl -> hd::tl |> List.reduce (+)) // <|>% (str "")
+// let unquotedField = pipe2 nonSpaceOrSep (many (allowedSpaces >>. nonSpaceOrSep)) (fun hd tl -> hd::tl |> List.reduce (+)) // <|>% (str "")
+let unquotedField = stringsSepBy nonSpaceOrSep allowedSpaces |>> (fun s -> s.TrimEnd([|' ';'\t'|])) <!> "unquoted field"
+// sepEndBy
 
 let csvValue, csvValueRef = createParserForwardedToRef()
 
-let line = sepBy csvValue (str ",")
+let line = sepBy csvValue (str_ws ",")
 
 do csvValueRef := choice[quotedField
-                         unquotedField]
+                         unquotedField] <!> "csvValueRef"
 
-let csv = sepBy line newline .>> ws .>> eof
+// "aaa,bbb"
+
+let csv = ws >>. sepBy line newline .>> ws .>> eof
 
 let parseString csvString =
     match run csv csvString with
