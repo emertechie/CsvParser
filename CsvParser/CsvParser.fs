8e2149c1e3b2fd4d2f3f0c97b2d231ed0c8ecc24
Andrew Smith <devnull@localhost>
2012-08-05 17:19:34 +0100

    Add ParseCharStream method

--- CsvParser/CsvParser.fs
+++ CsvParser/CsvParser.fs
@@ -1,7 +1,26 @@
 ﻿namespace CsvParser
 open CsvParserModule
+open System.IO
+open FParsec
 
 type public CsvParser() = 
     member x.Parse csv =
         let results = parseString csv
         results |> Seq.map (fun line -> line |> List.toArray)
+
+    member x.ParseStream stream encoding =
+        let results = parseStream stream encoding
+        results |> Seq.map (fun line -> line |> List.toArray)
+
+    member x.ParseCharStream charStream =
+        let results = parseCharStream charStream
+        results |> Seq.map (fun line -> line |> List.toArray)
+
+    member x.ParseFile fileName encoding =
+        use stream = File.OpenRead fileName
+        x.ParseStream stream (* fileName *) encoding
+
+    (*member x.ParseStream stream streamName encoding =
+        let results = runParserOnStream csv () stream encoding
+        results |> Seq.map (fun line -> line |> List.toArray)
+       *)
--- CsvParser/CsvParserModule.fs
+++ CsvParser/CsvParserModule.fs
@@ -16,11 +16,11 @@ let str_ws s = str s .>> ws
 
 let nonQuoteChars = manySatisfy (fun c -> c <> '"')
 let escapedQuote = stringReturn "\"\"" "\""
-let quotedField = between (str "\"") (str "\"") (stringsSepBy nonQuoteChars escapedQuote) .>> ws <!> "quoted field"
+let quotedField = between (str "\"") (str "\"") (stringsSepBy nonQuoteChars escapedQuote) .>> ws // <!> "quoted field"
 
-let nonSpaceOrSep = manySatisfy (function ','|'\n'|' '|'\t' -> false | _ -> true) <!> "nonSpaceOrSep"
-let allowedInterCharSpaces = many1Satisfy (function ' '|'\t' -> true | _ -> false) <!> "allowedSpaces"
-let unquotedField = stringsSepBy nonSpaceOrSep allowedInterCharSpaces |>> (fun s -> s.TrimEnd([|' ';'\t'|])) <!> "unquoted field"
+let nonSpaceOrSep = manySatisfy (function ','|'\n'|' '|'\t' -> false | _ -> true) // <!> "nonSpaceOrSep"
+let allowedInterCharSpaces = many1Satisfy (function ' '|'\t' -> true | _ -> false) // <!> "allowedSpaces"
+let unquotedField = stringsSepBy nonSpaceOrSep allowedInterCharSpaces |>> (fun s -> s.TrimEnd([|' ';'\t'|])) // <!> "unquoted field"
 //let unquotedField = manyCharsTill (noneOf ",\n") (pchar ',') << doesn't work because it consumes the comma
 
 let csvValue, csvValueRef = createParserForwardedToRef()
@@ -28,11 +28,38 @@ let csvValue, csvValueRef = createParserForwardedToRef()
 let line = sepBy csvValue (str_ws ",")
 
 do csvValueRef := choice[quotedField
-                         unquotedField] <!> "csvValueRef"
+                         unquotedField] // <!> "csvValueRef"
 
 let csv = ws >>. sepBy line newline .>> ws .>> eof
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let internal applyParser (parser: Parser<'Result,'UserState>) (stream: CharStream<'UserState>) =
+    let reply = parser stream
+    if reply.Status = Ok then
+        Success(reply.Result, stream.UserState, stream.Position)
+    else
+        let error = ParserError(stream.Position, stream.UserState, reply.Error)
+        Failure(error.ToString(stream), error, stream.UserState)
+
+let internal runParserOnStream (parser: Parser<'Result,'UserState>) (ustate: 'UserState) (byteStream: System.IO.Stream) (encoding: System.Text.Encoding) =
+    let leaveOpen = true
+    let stream = new CharStream<'UserState>(byteStream, leaveOpen, encoding)
+    stream.UserState <- ustate
+    // stream.Name <- streamName
+    applyParser parser stream
+
 let parseString csvString =
     match run csv csvString with
+        | Failure(errorMsg, _, _) -> raise (new Exception(sprintf "Parsing error: %s" errorMsg))
+        | Success(results, _, _) -> results
+
+let parseStream stream encoding =
+    match runParserOnStream csv () stream encoding with
+        | Failure(errorMsg, _, _) -> raise (new Exception(sprintf "Parsing error: %s" errorMsg))
+        | Success(results, _, _) -> results
+
+let parseCharStream charStream =
+    match applyParser csv charStream with
         | Failure(errorMsg, _, _) -> raise (new Exception(sprintf "Parsing error: %s" errorMsg))
         | Success(results, _, _) -> results
