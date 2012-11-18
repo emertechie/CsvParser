2f2f917d8fb35eeb363d78eb46302832fefccf63
Andrew Smith <devnull@localhost>
2012-11-18 12:10:37 +0000

    Add nuspec file and build script to build Nuget package

---
+++ build.ps1
@@ -0,0 +1,37 @@
+
+Properties {
+	$buildDir = Split-Path $psake.build_script_file	
+	$buildArtifactsDir = "$buildDir\_build"
+	$projectOutputDir = "$buildArtifactsDir\bin\"
+	$solutionDir = "$buildDir"
+	$version = "0.5.0"
+}
+
+Task default -Depends Compile, RunTests, CreatePackage
+
+Task Compile -Depends Clean {
+	Write-Host "Building solution" -ForegroundColor Green
+	Exec { msbuild "$solutionDir\CsvParser\CsvParser.fsproj" /t:Build /p:Configuration=Release /v:quiet /p:OutDir=$projectOutputDir } 
+}
+
+Task Clean {
+	Write-Host "Creating BuildArtifacts directory" -ForegroundColor Green
+	if (Test-Path $buildArtifactsDir) 
+	{	
+		rd $buildArtifactsDir -rec -force | out-null
+	}
+ 	  
+	mkdir $buildArtifactsDir | out-null
+	
+	Write-Host "Cleaning solution" -ForegroundColor Green
+	Exec { msbuild "$solutionDir\CsvParser.sln" /t:Clean /p:Configuration=Release /v:quiet } 
+}
+
+Task RunTests {
+	"TODO: Run tests"	
+}
+
+Task CreatePackage {
+	Write-Host "Building NuGet package" -ForegroundColor Green
+	&"$solutionDir\.nuget\nuget.exe" pack "$solutionDir\CsvParser.nuspec" -OutputDirectory "$buildArtifactsDir" -Version $version
+}
